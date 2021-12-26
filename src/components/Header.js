078dba0fa2fbd03e4ea58803121975344d3d8de6
OviYaD <69393959+OviYaD@users.noreply.github.com>
2021-12-26 11:09:28 +0530

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ export const Header = () => {
     return ( <
         div >
         <
-        h1 > Task tracker < /h1> </div >
-    )
-}
+        h1 > Task tracker < /h1>{" "} <
+        /div>
+    );
+};

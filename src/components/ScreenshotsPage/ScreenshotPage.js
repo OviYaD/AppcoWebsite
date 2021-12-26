a6820420e91e36bfa22561947a25577532e9c543
OviYaD <69393959+OviYaD@users.noreply.github.com>
2021-12-26 11:11:07 +0530

    updates

--- src/components/ScreenshotsPage/ScreenshotPage.js
+++ src/components/ScreenshotsPage/ScreenshotPage.js
@@ -4,13 +4,41 @@ import "./ScreenshotPage.css";
 class ScreenshotPage extends Component {
   render() {
     return (
-      <div>
+      <div className="ss">
         <div className="container-fluid">
           <div className="row">
-            <div className="col-md-4 lg-4 sm-12 xs-12 ">
-              <h3>
+            <div className="col-md-5 lg-5 sm-12 xs-12 stxt">
+              <h2>
                 Applic Apps <br /> Screenshot
-              </h3>
+              </h2>
+              <p>
+                Lorem ipsum dolor sit amet, consecadipiscing elit, sed do
+                eiusmod tempor incididunt ut ore et dolore magna aliqua. Quis
+                ipsum suspendisse gravida. Risus commodo viverra maecenasan
+                lacus vel facilisis.
+              </p>
+            </div>
+            <div className="col-md-7 lg-7 sm-12 xs-12 simg">
+              <img
+                class="img-fluid"
+                src="https://preview.colorlib.com/theme/appco/assets/img/gallery/xApp2.png.pagespeed.ic.JbwxSe9pMq.webp"
+                alt="logo"
+              />
+              <img
+                class="img-fluid"
+                src="https://preview.colorlib.com/theme/appco/assets/img/gallery/xApp2.png.pagespeed.ic.JbwxSe9pMq.webp"
+                alt="logo"
+              />
+              <img
+                class="img-fluid"
+                src="https://preview.colorlib.com/theme/appco/assets/img/gallery/xApp2.png.pagespeed.ic.JbwxSe9pMq.webp"
+                alt="logo"
+              />
+              <img
+                class="img-fluid"
+                src="https://preview.colorlib.com/theme/appco/assets/img/gallery/xApp2.png.pagespeed.ic.JbwxSe9pMq.webp"
+                alt="logo"
+              />
             </div>
           </div>
         </div>
@@ -18,3 +46,5 @@ class ScreenshotPage extends Component {
     );
   }
 }
+
+export default ScreenshotPage;
